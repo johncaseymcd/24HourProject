82b95e5c2d6dc9d47048f8996485011b30cb52db
Steve <slshotts77@gmail.com>
2021-02-05 17:35:02 -0500

    steve

--- 24HourProject.Data/User.cs
+++ 24HourProject.Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,4 +20,6 @@ namespace _24HourProject.Data
         [Display(Name = "Enter Email")]
         public string Email { get; set; }
     }
+
+    test
 }
